9ad6defd7e6f36a3757f08bf962a09128bda280a
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-10-21 12:00:15 +0300

    declared constructor

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 class Tomagotchi{
-
+    constructor(name){
+        this.name = name
+        // this.balance = balance
+        // this.account = accountID
+        }
 }
 
 let tomagotchi = new Tomagotchi()
